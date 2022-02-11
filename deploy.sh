8bc76b4ca0167a9bc1fc56937ffcea901f3c6df1
杨绅轩 <957355473@qq.com>
2022-02-11 19:50:05 +0800

    up to gitee

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yangshenxuan/morney-1-website.git master:gh-pages
+git push -f https://gitee.com/yang-shenxuan/morney.git master:gh-pages
 
 cd -
